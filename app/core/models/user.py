a0e43b2ef4bd101e8ddb3293ce15da3b071589f4
Oleksandr Sai <oleksandr.sai@nikel.com.ua>
2025-09-24 23:36:16 +0300

    upd user fileds in model

--- app/core/models/user.py
+++ app/core/models/user.py
@@ -5,12 +5,13 @@ from .base import Base
 
 
 class User(Base):
-    name: Mapped[str] = mapped_column(String(30))
-    fullname: Mapped[Optional[str]]
+    login: Mapped[str] = mapped_column(String(255))
+    password: Mapped[str] = mapped_column(String(255))
+    name: Mapped[Optional[str]]
 
     bids: Mapped[List["Bid"]] = relationship(
         back_populates="user", cascade="all, delete-orphan"
     )
 
     def __repr__(self) -> str:
-        return f"User(id={self.id!r}, name={self.name!r}, fullname={self.fullname!r})"
+        return f"User(id={self.id!r}, login={self.login!r}, name={self.name!r})"
